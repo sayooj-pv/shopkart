3164e30509dfe10482f602ea058c3944d1c1d221
SAYOOJ P V <90050844+sayooj-pv@users.noreply.github.com>
2025-11-25 21:42:42 +0530

    Add files via upload

---
+++ shopcart/shop/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+ 
+urlpatterns= [
+    path('',views.home,name="home"),
+    path('register',views.register,name="register"),
+    path('login',views.login_page,name="login"),
+    path('logout',views.logout_page,name="logout"),
+    path('cart',views.cart_page,name="cart"),
+    path('fav',views.fav_page,name="fav"),
+    path('favviewpage',views.favviewpage,name="favviewpage"),
+    path('remove_fav/<str:fid>',views.remove_fav,name="remove_fav"),
+    path('remove_cart/<str:cid>',views.remove_cart,name="remove_cart"),
+    path('collections',views.collections,name="collections"),
+    path('collections/<str:name>',views.collectionsview,name="collections"),
+    path('collections/<str:cname>/<str:pname>',views.product_details,name="product_details"),
+    path('addtocart',views.add_to_cart,name="addtocart"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("create_order/", views.create_order, name="create_order"),
+    path("payment_success/", views.payment_success, name="payment_success"),
+
+]
